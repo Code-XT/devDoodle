a6a7091a80ead81b9862d38d70dd989a29aaaf85
Code-XT <rahuljhabokaro123@gmail.com>
2024-07-06 17:04:55 +0530

    slight color correction

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        backgroundStart: "rgb(var(--background-start-rgb))",
+        backgroundEnd: "rgb(var(--background-end-rgb))",
+      },
+    },
   },
   plugins: [],
 };
